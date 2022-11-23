69135999152b7ca83505209e7ff6e75d6f62b07a
WT <wilsonwttran@gmail.com>
2022-11-22 17:32:24 -0800

    Working webpack config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import {createRoot} from 'react-dom/client'
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('div')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('div')
+// );
  
+const container = document.getElementById('div');
+const root = createRoot(container);
+root.render(<App />);
