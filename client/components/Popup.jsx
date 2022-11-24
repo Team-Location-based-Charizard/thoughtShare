df2c648f9fe148967ed15cbbede426b0adc99974
li.zihan94@gmail.com <li.zihan94@gmail.com>
2022-11-23 19:27:02 -0800

    update popup

--- client/components/Popup.jsx
+++ client/components/Popup.jsx
@@ -2,26 +2,16 @@ import React from "react";
 import { useState } from "react";
 
 const Popup = props => {
-  const [open, setOpen] = useState(props.open)
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClickClose = () => {
-    setOpen(false);
-  };
-
-  if (open){
   return(
     <div className="popup-container">
       <div className="popup">
-        <span className="close-box" onClick={handleClickClose}>x</span>
+        <span className="close-box" onClick={props.onClose}> x </span>
         {props.text}
       </div>
     </div>
   )
   }
-}
+
 
 export default Popup;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,39 @@
 import React, { useState } from 'react';
+import { render } from 'react-dom';
 import Popup from '../components/Popup.jsx';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
 
-  const togglePopup = () => {
-    setIsOpen(!isOpen);
+  const [open, setOpen] = useState(false)
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClickClose = () => {
+    setOpen(false);
+  };
+  
+
+  let popup = null;
+  if (open){
+    popup = <Popup text={<>
+      <b>My thought 1</b>
+      <p>Helloooooooooooooooo</p>
+    </>} onClose = {handleClickClose} />
   }
 
-  return <div>
-    <input
-      type="button"
+  return(
+    <div>
+    <input type="button"
       value="Click to Open Popup"
-      onClick={togglePopup}
-    />
-    {isOpen && <Popup
-      text={<>
-        <b>My thought</b>
-        <p>Helloooooooooooooooo</p>
-        <button>Test button</button>
-      </>}
-      open={true}
-    />}
-  </div>
+      onClick={handleClickOpen}
+    /> 
+    {popup}
+    </div>
+  )
 }
+  
+
+  
+
 
 export default App;
