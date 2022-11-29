b9241b5959bf3eea258e3c400c73d764880b8412
Your Name <tonfroy90@gmail.com>
2022-11-28 17:37:12 -0800

    Monday commit with root folder

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client'
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
- 
-const container = document.getElementById('div');
-const root = createRoot(container);
-root.render(<App />);
