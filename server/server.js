4c690b7aa1233685f217492a6c7f62645b3d187d
Wilson-Tran <100881978+Wilson-Tran@users.noreply.github.com>
2022-11-28 17:59:47 -0800

    Revert "Wt/express server"

--- server/server.js
+++ server/server.js
@@ -1,45 +1,18 @@
 const express = require('express');
-const cors = require('cors');
-
-const path = require('path');
+const app = express();
+const router = require('/routes/router');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 //Import Controllers
 
 // Import Routers
-const authRouter = require('./routes/authRouter');
-const thoughtRouter = require('./routes/thoughtRouter');
 
-const app = express();
-const db = require('./models/userThoughtsModels');
-
-app.use(cors());
+const db = require('./')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('*/api/auth', authRouter);
-app.use('*/api/thoughts', thoughtRouter);
+app.use('/', router);
 
-// 404 error
-app.use('/*', (req, res) => {
-  return res.sendStatus(404);
-});
-
-// Express global error handler
-app.use((err, req, res, next) => {
-  const defaultErr = {
-    log: 'Express error handler caught unknown middleware error', 
-    status: 500,
-    message: { err: 'A error occurred' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log, ' ', err);
-  return res.status(errorObj.status).json(err);
-});
-
-// Start server
-app.listen(process.env.PORT, () => {
-  console.log(`TEST Server listening on port: ${process.env.PORT}`);
-})
+app.use
