c00e099e760e473f31962a97be68fdde4abf7e11
Your Name <tonfroy90@gmail.com>
2022-11-29 19:26:16 -0800

    Preparing PR

--- client/src/component/LandingPage.jsx
+++ client/src/component/LandingPage.jsx
@@ -1,22 +1,105 @@
-import * as React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Nav from "./Nav";
 import MapContainer from "./Map";
 // import { useEffect, useState } from "react";
 
 const LandingPage = () => {
+  const [createUserID, setCreateUserID] = useState("");
+  const [createThought, setCreateThought] = useState("");
+  const [createLat, setCreateLat] = useState("");
+  const [createLng, setCreateLng] = useState("");
+  
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      return "hello";
+    }
+  }
+
+  function showPosition(position) {
+    console.log(position)
+  }
+
+  getLocation()
+
+  const handleSubmit = (e) => {
+    
+    const userID = createUserID
+    const thought = createThought
+
+    console.log("user ID: " + userID)
+    console.log("createThough: " + thought)
+    // let lat = showPosition()
+    // let lng = showPosition()
+    // const lat = createLat
+    // const lng = createLng
+
+    // function getLocation() {
+    //   if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(showPosition);
+    //   } else {
+    //     return "hello";
+    //   }
+    // }
+  
+    // function showPosition(position) {
+    //   console.log(position)
+    //   // lat = position.coords.latitude
+    //   // lng = position.coords.longitude
+    //   // return position
+    // }
+
+    // getLocation()
+    
+    // // console.log("handleHost post called");
+
+    // // getLocation()
+    
+    // // console.log(lat)
+    // console.log(lng)
+    axios
+      .post(
+        "/api/thoughts/addThought",
+        {
+          user_id: userID,
+          thought: thought,
+          // lat: lat,
+          // lng: lng
+        },
+      )
+      .then((res) => {
+        console.log("response from axios:", res);
+        if (res.status === 200) {
+          alert("thought & userID created");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
       <Nav />
-
       <div className=" bg-slate-400 h-screen flex flex-col justify-center items-center space-y-4">
-        <form>
-          <label>
-            <h1 className="text-3xl text-white font-bold pb-4">
-              What Are They Thinking?
-            </h1>
-            <input type="text" value="Zip Code" />
-          </label>
-        </form>
+        <div className="bg-gradient-to-bl from-[#032a267f] to-[#181919] w-[30%] h-[30%] text-center flex flex-col mt-16 rounded-xl shadow-2x border-2 border-[#181919] ">
+          <h1 className="text-3xl text-white font-bold pb-4">
+            What Are They Thinking?
+          </h1>
+          <form>
+            <label for="userID">User ID </label>
+            <input type="text" id="userID" name="userID" onChange={(e) => setCreateUserID(e.target.value)} defaultValue="" /><br/><br/>
+            <label for="fname">What Are You Thinking? </label>
+            <input type="text" id="fname" name="fname" onChange={(e) => setCreateThought(e.target.value)} defaultValue=""/><br />
+          </form>
+          <button
+            className="p-4 w-[50%] flex justify-items-center rounded-lg border-5 bg-slate-200 border-white "
+            onClick={handleSubmit}
+          >
+            Testing button
+          </button>
+        </div>
+
         <div className=" box-content border-[#469494] border-8 w-[50%] h-[50%]">
           <MapContainer />
         </div>
--- client/src/component/Map.jsx
+++ client/src/component/Map.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 import { mapStyles } from "../assets/mapStyles";
 
 const containerStyle = {
@@ -13,6 +13,68 @@ const mapStyle = {
 };
 
 class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [
+        {
+          title: "The marker`s title will appear as a tooltip.",
+          name: "SOMA",
+          position: { lat: 37.778519, lng: -122.40564 },
+        },
+      ],
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.showPosition);
+    } else {
+      return "hello";
+    }
+  };
+
+  showPosition(t, map, position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    this.setState((previousState) => {
+      return {
+        markers: [
+          ...previousState.markers,
+          {
+            title: "heyyy",
+            name: "",
+            position: { lat, lng },
+          },
+        ],
+      };
+    });
+  }
+  // showPosition(position) {
+  //   // Recenter gmap to following
+  //   lat = position.coords.latitude
+  //   long = position.coords.longitude
+  // }
+
+  onClick(t, map, coord) {
+    const { latLng } = coord;
+    const lat = latLng.lat();
+    const lng = latLng.lng();
+    this.setState((previousState) => {
+      return {
+        markers: [
+          ...previousState.markers,
+          {
+            title: "heyyy",
+            name: "",
+            position: { lat, lng },
+          },
+        ],
+      };
+    });
+  }
+
   render() {
     return (
       <Map
@@ -24,7 +86,20 @@ class MapContainer extends Component {
           lat: 34.052235,
           lng: -118.243683,
         }}
-      />
+        onDblclick={this.onClick}
+      >
+        
+        {this.state.markers.map((marker, index) => (
+          <Marker
+            key={index}
+            title={marker.title}
+            name={marker.name}
+            position={marker.position}
+          />
+        ))}
+        
+        <Marker />
+      </Map>
     );
   }
 }
@@ -32,64 +107,3 @@ class MapContainer extends Component {
 export default GoogleApiWrapper({
   apiKey: "AIzaSyAUTgzsq1-33AaJ7larbKfO-R2coYC4-ac",
 })(MapContainer);
-
-//////// OLD CODE //////
-/* import { Wrapper, Status } from "@googlemaps/react-wrapper";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-import { mapStyles } from './mapStyles.js'
-// const ref = React.useRef(null);
-// const [map, setMap] = React.useState();
-
-// React.useEffect(() => {
-//   if (ref.current && !map) {
-//     setMap(new window.google.maps.Map(ref.current, {}));
-//   }
-// }, [ref, map]);
-
-const containerStyle = {
-  width: "100%",
-  height: "100%",
-  float: "left",
-};
-
-const options = {
-  styles: mapStyles,
-};
-
-const Map = ({ data, zoom }) => {
-  const center = {
-    lat: 0,
-    lng: 0,
-  };
-  // const listings = data.listings;
-
-  // const markerElems = listings.map((listings, i) => {
-  //   const position = {
-  //     lat: listings.coordinates.lat,
-  //     lng: listings.coordinates.lng,
-  //   };
-
-  //   const onMarkerClick = (e) => {
-  //     console.log(listings.address);
-  //   };
-
-  //   return <Marker onClick={onMarkerClick} key={i} position={position} />;
-  // });
-  const key = "AIzaSyAUTgzsq1-33AaJ7larbKfO-R2coYC4-ac"
-  return (
-    <LoadScript googleMapsApiKey={key}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={8}
-        options={options}
-      >
-        {/* {markerElems} */
-// <></>
-//       </GoogleMap>
-//       <h1 className="text-6xl text-black">Are you even working?</h1>
-//     </LoadScript>
-//   );
-// };
-// export default Map;
-// */
